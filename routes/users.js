9b65620b57477dd2dcef7f324d7c9e4214c1f583
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-07-29 14:36:04 +0530

    not so fresh app; many additions @ views/index

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({}, {}, function(err, docs){
+    res.json(docs);
+  });
+});
+
+module.exports = router;
