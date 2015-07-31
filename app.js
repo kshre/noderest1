7717dd6f13c3c5acf59c23cce2e560f9475e17e6
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-07-31 21:52:03 +0530

    router has db access

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/noderest1');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// make database object available to the router
+// not optimal enough, but it works
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
