1fa22d3f7feb10bdbbc3a134ce33adf909bf5d84
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-07 07:33:05 +0530

    User Info populates the User Info card in view

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 
 	// Populate user table on page load
 	populateTable();
-
+	
+	// Username link click
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 // Function ========================================
@@ -18,6 +20,8 @@ function populateTable() {
 	
 	// jQuery AJAX call for JSON
 	$.getJSON('users/userlist', function( data ) {
+		// Store the recieved user data array into the variable in the global object
+		userListData = data;	
 		
 		// For each item in the JSON, add a table row and cells to the content string
 		$.each(data, function() {
@@ -32,3 +36,28 @@ function populateTable() {
 		$('#userList table tbody').html(tableContent);
 	});	
 };
+
+// Show User Info
+function showUserInfo(event) {
+
+	// Prevent link from firing
+	event.preventDefault();
+
+	// Retrive username from link rel attribute
+	var thisUsername = $(this).attr('rel');
+
+	// Get index of object based on id value
+	var arrayPosition = userListData.map(function( arrayItem ) { return arrayItem.username }).indexOf(thisUsername);
+
+	// Get object based on the index calculated above
+	var thisUserObject = userListData[arrayPosition];
+
+	// Populate Info Box
+	$('#userInfoName').text(thisUserObject.fullname);
+	$('#userInfoAge').text(thisUserObject.age);
+	$('#userInfoGender').text(thisUserObject.gender);
+	$('#userInfoLocation').text(thisUserObject.location);
+
+};	
+
+
